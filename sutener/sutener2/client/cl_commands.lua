fe52ac06fa093eef05757aa0a7e2290a5db0130b
ageggi <gekkiev7755@gmail.com>
2025-06-17 06:15:20 +0300

    Delete sutener/sutener2 directory

--- sutener/sutener2/client/cl_commands.lua
+++
@@ -1,9 +0,0 @@
-CreateThread(function()
-    RegisterCommand('sutener', function()
-        if not IsPedInAnyVehicle(PlayerPedId(), false) then
-            exports['qb-core']:GetCoreObject().Functions.Notify('Нужно быть в машине!', 'error')
-            return
-        end
-        TriggerServerEvent('qb-sutenerjob:startWork')
-    end)
-end)
