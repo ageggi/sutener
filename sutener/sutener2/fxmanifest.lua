25e8069a8da41b541abea2b0f55689d549b637d0
ageggi <gekkiev7755@gmail.com>
2025-06-17 06:12:24 +0300

    Add files via upload

--- sutener/sutener2/fxmanifest.lua
+++ sutener/sutener2/fxmanifest.lua
@@ -2,18 +2,12 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'ageggi'
-description 'sutener job (qbcore/ox_inventory ready)'
-version '1.0.1'
+description 'sutener job (clean rewrite, qbcore/ox_inventory ready)'
+version '2.0.0'
 
 shared_script 'config.lua'
 
-client_scripts {
-    'client/cl_main.lua',
-    'client/cl_commands.lua'
-}
-
-server_scripts {
-    'server/sv_main.lua'
-}
+client_script 'client.lua'
+server_script 'server.lua'
 
 lua54 'yes'
